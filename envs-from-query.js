4ae2a622701e60c75e1f63b42bb66b1149980637
Ralph Cowling <speak@ralph-cowling.com>
2016-09-09 18:00:17 +0100

    wip save to file

---
+++ envs-from-query.js
@@ -0,0 +1,58 @@
+module.exports = {
+  dvcus01:
+   [ '{"application":"EVW-CUSTOMER","version":"1.0.2_288-1"}',
+     '{"application":"Flight Forecast Service","version":"flight-forecast-service-version=1.0-77"}',
+     '{"application":"EVW Self Serve","version":"build-master-2016-09-08-500"}',
+     '{"application":"EVW-APPLICATION-PROCESSOR","version":"1.0.0_223-1"}' ],
+  dvcus02:
+   [ '{"application":"EVW-CUSTOMER","version":"1.0.2_288-1"}',
+     '{"application":"Flight Forecast Service","version":"flight-forecast-service-version=1.0-77"}',
+     '{"application":"EVW Self Serve","version":"build-master-2016-09-08-500"}',
+     '{"application":"EVW-APPLICATION-PROCESSOR","version":"1.0.0_223-1"}' ],
+  utcus01:
+   [ '{"application":"EVW-CUSTOMER","version":"1.0.2_285-1"}',
+     'The requested resource could not be found.',
+     '{"application":"EVW Self Serve","version":"build-master-2016-09-08-500"}',
+     '{"application":"EVW-APPLICATION-PROCESSOR","version":"1.0.0_223-1"}' ],
+  ppcus01:
+   [ '{"application":"EVW-CUSTOMER","version":"1.0.2_283-1"}',
+     'The requested resource could not be found.',
+     '{"application":"EVW Self Serve","version":"build-master-2016-09-08-500"}',
+     '{"application":"EVW-APPLICATION-PROCESSOR","version":"1.0.0_223-1"}' ],
+  ppcus02:
+   [ '{"application":"EVW-CUSTOMER","version":"1.0.2_283-1"}',
+     'The requested resource could not be found.',
+     '{"application":"EVW Self Serve","version":"build-master-2016-09-08-500"}',
+     '{"application":"EVW-APPLICATION-PROCESSOR","version":"1.0.0_223-1"}' ],
+  pdcus01:
+   [ '{"application":"EVW-CUSTOMER","version":"1.0.2_283-1"}',
+     'The requested resource could not be found.',
+     '{"application":"EVW Self Serve","version":"build-master-2016-09-08-500"}',
+     '{"statusCode":404,"error":"Not Found"}' ],
+  pdcus02:
+   [ '{"application":"EVW-CUSTOMER","version":"1.0.2_283-1"}',
+     'The requested resource could not be found.',
+     '{"application":"EVW Self Serve","version":"build-master-2016-09-08-500"}',
+     '{"statusCode":404,"error":"Not Found"}' ],
+  dvcas01:
+   [ '{"application":"caseworker","version":"1.0.0_1325-1"}',
+     '{"application":"EVW Integration Service","version":"1.0.0_774-1"}' ],
+  dvcas02:
+   [ '{"application":"caseworker","version":"1.0.0_1325-1"}',
+     '{"application":"EVW Integration Service","version":"1.0.0_774-1"}' ],
+  utcas01:
+   [ '{"application":"caseworker","version":"1.0.0_1323-1"}',
+     '{"application":"EVW Integration Service","version":"akka.version = 2.4.4"}' ],
+  ppcas01:
+   [ '{"application":"caseworker","version":"1.0.0_1323-1"}',
+     '{"application":"EVW Integration Service","version":"1.0.0_774-1"}' ],
+  ppcas02:
+   [ '{"application":"caseworker","version":"1.0.0_1323-1"}',
+     '{"application":"EVW Integration Service","version":"1.0.0_774-1"}' ],
+  dvocr01: [ '* Name : Passport OCR Service\n* Version : 1.0.0_37-1' ],
+  dvocr02: [ '* Name : Passport OCR Service\n* Version : 1.0.0_37-1' ],
+  utocr01: [ '{ "name" : "Passport OCR Service",\n"version" : "akka.version = 2.4.2' ],
+  ppocr01: [ '{ "name" : "Passport OCR Service",\n"version" : "akka.version = 2.4.2' ],
+  ppocr02: [ '{ "name" : "Passport OCR Service",\n"version" : "akka.version = 2.4.2' ]
+};
+
--- index.js
+++ index.js
@@ -1,67 +1,70 @@
 'use strict';
 
+const fs = require('fs');
 const http = require('http');
 const shttp = require('socks5-http-client');
 const async = require('async');
 const local = process.env.VERSIONS_APP_LOCAL;
-
-// const cheerio = require('cheerio');
-// const authstr = process.env.NAGIOS_AUTH;
-// const base = (host) => `monitoring.registered-traveller.homeoffice.gov.uk/nagios/cgi-bin/extinfo.cgi?type=2&host=${host}&service=Version+-+`;
-
-let boxes = {};
-
-let calls = [];
 const apps = {
-  customer: {
+  'evw-customer': {
     box: 'cus',
     port: 3018,
     endpoint: '/shared/healthcheck/version'
   },
-  flights: {
+  'flights-forecast-service': {
     box: 'cus',
     port: 9350,
     endpoint: '/version'
   },
-  selfserve: {
+  'evw-self-serve': {
     box: 'cus',
     port: 3016,
     endpoint: '/version'
   },
-  processor: {
+  'evw-application-processor': {
     box: 'cus',
     port: 8897,
     endpoint: '/version'
   },
-  caseworker: {
+  'evw-caseworker': {
     box: 'cas',
     port: 9000,
     endpoint: '/caseworker/version'
   },
-  integration: {
+  'evw-integration-service': {
     box: 'cas',
     port: 9300,
     endpoint: '/version'
+  },
+  'passport-ocr-service': {
+    box: 'ocr',
+    port: 9360,
+    endpoint: '/healthcheck'
   }
-  // ,
-  // ocr: {
-  //   box: 'ocr',
-  //   port: 9360,
-  //   endpoint: '/healthcheck'
-  // }
 };
 
-// const donecb = (err, res, host) => {
-//   if(err) {
-//     console.log('BAILED', err);
-//   }
+let envs = {};
+let calls = [];
 
-//   console.log('GOT', res);
-//   (boxes[host] = boxes[host] || []).push(res);
-//   console.log(boxes);
-// };
+const build = ((item) => {
+  console.log('building', item);
+  let appValue;
+  let app = {};
 
-const query = (host, port, path, cb) => {
+  try {
+    appValue = JSON.parse(item.result);
+  } catch (e) {
+    appValue = '';
+  }
+
+  app[item.application] = appValue;
+  envs[item.host] = Object.assign({},
+    envs[item.host],
+    app
+  );
+});
+
+const query = (host, port, path, app, cb) => {
 
   let curler = !!local ? shttp : http;
   let location = {
@@ -88,11 +91,23 @@ const query = (host, port, path, cb) => {
       if (err) {
         console.log('err', err);
       }
+
       console.log('got', body, res.statusCode);
+      build({
+        result: body,
+        application: app,
+        host: host,
+        query: `${host}:${port}${path}`,
+        code: res.statusCode
+      });
+
       cb(err, body, host);
-    }).on('error', (e) => {
-      console.log(`Got error querying: ${e.message}`);
+    }).on('error', (err) => {
+      console.log(`Got error querying: ${err.message}`);
+      cb(err);
     });
+  }).setTimeout(400, () => {
+    cb('timed out querying ' + host);
   });
 };
 
@@ -102,173 +117,51 @@ const chainCalls = (apps, calls, next) => {
     console.log(`adding... ${key}:`, apps[key], `dv${app.box}01`);
     // lol, do this better
     calls.push((cb) => {
-      query(`dv${app.box}01`, app.port, app.endpoint, cb);
+      query(`dv${app.box}01`, app.port, app.endpoint, key, cb);
     });
     calls.push((cb) => {
-      query(`dv${app.box}02`, app.port, app.endpoint, cb);
+      query(`dv${app.box}02`, app.port, app.endpoint, key, cb);
     });
     calls.push((cb) => {
-      query(`ut${app.box}01`, app.port, app.endpoint, cb);
+      query(`ut${app.box}01`, app.port, app.endpoint, key, cb);
     });
     calls.push((cb) => {
-      query(`pp${app.box}01`, app.port, app.endpoint, cb);
+      query(`pp${app.box}01`, app.port, app.endpoint, key, cb);
     });
-    // calls.push((cb) => {
-    //   query(`pp${app.box}02`, app.port, app.endpoint, cb);
-    // });
-    // calls.push((cb) => {
-    //   query(`pd${app.box}01`, app.port, app.endpoint, cb);
-    // });
-    // calls.push((cb) => {
-    //   query(`pd${app.box}02`, app.port, app.endpoint, cb);
-    // });
+    calls.push((cb) => {
+      query(`pp${app.box}02`, app.port, app.endpoint, key, cb);
+    });
+    calls.push((cb) => {
+      query(`pd${app.box}01`, app.port, app.endpoint, key, cb);
+    });
+    calls.push((cb) => {
+      query(`pd${app.box}02`, app.port, app.endpoint, key, cb);
+    });
   });
   next(calls);
 };
 
+const saveEnvs = (envs) => {
+
+  fs.writeFile('envs-saved.json', JSON.stringify(envs), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log('envs saved');
+  });
+
+};
+
 const makeCalls = (calls) => {
   async.parallel(async.reflectAll(calls), function(err, results) {
-    console.log('🙈 noooo err 💀', err);
-    console.log('final results:', results);
-    boxes = results.reduce((previous, current) => {
-      // console.log('previous', previous, 'current', current);
-      if(typeof current.value[0] !== 'undefined') {
-
-        let app = current.value[0];
-        let key = current.value[1];
-
-        console.log('assigning', app, 'to', current.value[1]);
-        (previous[key] = previous[key] || []).push(app);
-        // console.log('now looks like', previous);
-      }
-      return previous;
-    }, {});
-    console.log('boxes', boxes);
+    console.log('🙈 any errors? 💀', err);
+    console.log('final results', results);
+    console.log('envs', envs);
+    saveEnvs(envs);
   });
 };
 
 chainCalls(apps, calls, makeCalls);
-
-
-// let cus = [
-//   'evw-application-processor',
-//   'evw-customer',
-//   'evw-self-serve',
-//   'flight-forecast-service',
-//   'mock-integration-service'
-// ];
-
-// let cas = [
-//   'evw-caseworker',
-//   'evw-integration-service',
-// ];
-
-// let ocr = [
-//   'Passport+OCR+Service'
-// ];
-
-// let wps = [
-//   'rtp-worldpay-stub'
-// ];
-
-// let envs = {
-//   cus: cus,
-//   cas: cas,
-//   ocr: ocr,
-//   wps: wps
-// }
-
-// let hosts = {};
-
-// let levels = [
-//   'dv',
-//   'ut',
-//   'pd',
-//   'pp'
-// ];
-
-// http://monitoring.registered-traveller.homeoffice.gov.uk/nagios/cgi-bin/extinfo.cgi?type=2&host=dvcas02&service=Version+-+evw-caseworker
-
-// const envCheck = (env, list) => {
-//   let tasks = [];
-//   list.forEach((item) => {
-//     let it = {};
-//     it[item] = function() {
-//       this[`dv${env}01`] = check(`dv${env}01`, item, pick);
-//       // check(`dv${env}02`, item, pick),
-
-//       // check(`ut${env}01`, item, pick),
-//       // check(`ut${env}02`, item, pick),
-
-//       // check(`pp${env}01`, item, pick),
-//       // check(`pp${env}01`, item, pick),
-
-//       // check(`pd${env}02`, item, pick),
-//       // check(`pd${env}02`, item, pick)
-//     };
-//     tasks.push(it);
-//   });
-//   console.log(tasks);
-//   // async.parallel(tasks, (err, results) => {
-//   //   console.log(err, results);
-//   //   return doLog();
-//   // });
-// };
-
-// const check = (host, app, cb) => {
-//   let ping = `http://${authstr}@${base(host)}${app}`;
-//   // console.log(`querying ${ping}`);
-//   http.get(ping, (res) => {
-//     let body;
-//     res.on('data', (d) => {
-//       body += d;
-//     }).on('end', () => {
-//       cb(body, host, app);
-//     }).on('error', (e) => {
-//       console.log(`Got error querying ${base(host)}${app}: ${e.message}`);
-//     });
-//   });
-// }
-
-// const pick = (data, host, app) => {
-//   let $ = cheerio.load(data);
-//   let version = $('.stateInfoTable1').find('td:contains("Status Information:")').next().text();
-
-//   if(!version){
-//     return;
-//   }
-
-//   let entry = {};
-//   entry[app] = version;
-
-//   if (hosts.hasOwnProperty(host)) {
-//     // console.log(host, 'exists, pushing', entry);
-//     hosts[host][app] = version;
-//   } else {
-//     hosts[host] = {};
-//     hosts[host][app] = version;
-//     // console.log('created', host, 'pushing', entry);
-//   }
-//   // console.log('pushing', entry, 'into', hosts[host]);
-// }
-
-
-// const begin = () => {
-//   Object.keys(envs).forEach((item, i, arr) => {
-//     let list = envs[item];
-//     envCheck(arr[i], list);
-//   });
-// }
-
-// const doLog = () => console.log(hosts);
-
-// begin();
-//
-
-
-// setTimeout(function(){
-//   console.log(hosts);
-// },10000);
 
 
 
