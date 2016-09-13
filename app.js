5af67f7f0e1bff4d1f01b3e42c3231755eccf64e
Ralph Cowling <speak@ralph-cowling.com>
2016-09-13 11:55:19 +0100

    Simplify socket 🔌
    - just broadcast, and let us know when someone joins/disconnects

--- app.js
+++ app.js
@@ -9,18 +9,33 @@ const data = require('./envs-saved.json');
 const map = require('./envmap');
 const Socket = require('koa-socket');
 const io = new Socket();
-const co = require('co');
 const getter = require('./index');
-let Handlebars = require('handlebars');
-let content = map(data);
-let helpers = {
+
+const content = map(data);
+
+const helpers = {
   box: (obj, key, prop, num, app) => {
-    // console.log('list of rendering args', obj, key, prop, num, app);
     let lookup = `${key}${prop}${num}`;
     // console.log('and app from lookup', lookup, obj[lookup] && obj[lookup][app]);
     return obj[lookup] && obj[lookup][app] && obj[lookup][app].version;
   }
 };
+
+const getEnvs = (current) => {
+  getter.chain(
+    getter.apps,
+    getter.calls,
+    getter.make
+  ).then((data) => {
+    if( JSON.stringify(current) !== JSON.stringify(data) ) {
+      console.log('fresh envs', data);
+      io.broadcast( 'envs', 'release the bats' );
+      // todo transmit new environment dataset hbs.render
+    }
+  }).catch(function(e) {
+    console.log('error getting envs', e);
+  });
+}
 
 app.use(serve({
   rootDir: path.resolve(__dirname) + '/static'
@@ -39,71 +54,21 @@ app.use(function *() {
   });
 });
 
-io.attach(app)
+io.attach(app);
 
-/**
- * Socket middlewares
- */
-
-io.use( co.wrap( function *( ctx, next ) {
-  console.log( 'Socket middleware' )
-  const start = new Date
-  yield next()
-  const ms = new Date - start
-  console.log( `socket process took ${ ms }ms` )
-}))
-io.use( co.wrap( function *( ctx, next ) {
-  ctx.teststring = 'test'
-  yield next()
-}))
-
-/**
- * Socket handlers
- */
-io.on( 'connection', ctx => {
-  console.log( 'Join event', ctx.socket.id )
+io.on('connection', ctx => {
+  console.log( 'Join event', ctx.socket.id );
   io.broadcast( 'connections', {
     numConnections: io.connections.size
-  })
-})
+  });
+});
 
-io.on( 'disconnect', ctx => {
-  console.log( 'leave event', ctx.socket.id )
-  io.broadcast( 'connections', {
+io.on('disconnect', ctx => {
+  console.log('leave event', ctx.socket.id );
+  io.broadcast('connections', {
     numConnections: io.connections.size
-  })
-})
-io.on( 'data', ( ctx, data ) => {
-  console.log( 'data event', data )
-  console.log( 'ctx:', ctx.event, ctx.data, ctx.socket.id )
-  console.log( 'ctx.teststring:', ctx.teststring )
-  ctx.socket.emit( 'response', {
-    message: 'response from server'
-  })
-})
-io.on( 'ack', ( ctx, data ) => {
-  console.log( 'data event with acknowledgement', data )
-  ctx.acknowledge( 'received' )
-})
-io.on( 'numConnections', packet => {
-  console.log( `Number of connections: ${ io.connections.size }, ${ packet }` )
-})
-
-const getEnvs = (current) => {
-  getter.chain(
-    getter.apps,
-    getter.calls,
-    getter.make
-  ).then((data) => {
-    if( JSON.stringify(current) !== JSON.stringify(data) ) {
-      console.log('fresh envs', data);
-      io.broadcast( 'envs', 'release the bats' );
-      // todo transmit new environment dataset hbs.render
-    }
-  }).catch(function(e) {
-    console.log('error getting envs', e);
-  });;
-}
+  });
+});
 
 app.listen(app.listen(process.env.PORT || 3000), () => {
   setInterval(function () {
